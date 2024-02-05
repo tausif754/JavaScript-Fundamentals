4bc3e513f44d6ab4bfcac4c072f988e56ff8007d
Tausif Ahmad <tausifahmad754@gmail.com>
2024-02-05 16:33:11 +0530

    question-2 and question-3 solved

--- Question_2.js
+++ Question_2.js
@@ -3,3 +3,11 @@ Q2. Create a JavaScript program that demonstrates the basic usage of regular exp
 that takes a regex pattern and a string as input and returns true if there is a match, and false otherwise. Test
 the function with various patterns and strings.
 */
+
+function testRegex(pattern, inputString) {
+  let regex = new RegExp(pattern);
+  return regex.test(inputString);
+}
+console.log(testRegex(/[0-9]+/, "123")); //true
+console.log(testRegex(/[a-z]+/, "TAUSIF"));
+console.log(testRegex(/[a-z]+/, "tausif"));
--- Question_3.js
+++ Question_3.js
@@ -3,3 +3,29 @@ Q3. Write a JavaScript program that demonstrates the use of character classes in
 Create a function that searches for specific character classes in a given string and returns the matches. Test
 the function with patterns for digits, uppercase letters, lowercase letters, and special characters.
 */
+
+function findCharacterClasses(inputString) {
+  let digitPattern = /\d/g;
+  let uppercasePattern = /[A-z]/g;
+  let lowercasePattern = /[a-z]/g;
+  let specialCharPattern = /[^a-zA-Z0-9]/g;
+
+  let digits = inputString.match(digitPattern) || [];
+  let uppercaseLetters = inputString.match(uppercasePattern) || [];
+  let lowercaseLetters = inputString.match(lowercasePattern) || [];
+  let specialCharacter = inputString.match(specialCharPattern) || [];
+  return {
+    digits: digits,
+    uppercaseLetters: uppercaseLetters,
+    lowercaseLetters: lowercaseLetters,
+    specialCharacter: specialCharacter,
+  };
+}
+let inputString = "Hello123! world";
+let result = findCharacterClasses(inputString);
+
+console.log("Original String:", inputString);
+console.log("Digits", result.digits);
+console.log("Uppercase letters:", result.uppercaseLetters);
+console.log("Lowercase letters:", result.lowercaseLetters);
+console.log("Special characters:", result.specialCharacter);
